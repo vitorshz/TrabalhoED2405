88ce738b85781c55ad1a4096e9eb648509edfd60
vitorshz <110640686+vitorshz@users.noreply.github.com>
2023-05-24 21:23:09 -0300

    a

---
+++ TrabalhoED/src/main/java/br/unipar/trabalhoed/TrabalhoED.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package br.unipar.trabalhoed;
+
+/**
+ *
+ * @author lucia
+ */
+public class TrabalhoED {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
